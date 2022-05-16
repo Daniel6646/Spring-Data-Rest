8213bd2f85733ea4f77a039b3f9ea74ccd680dc3
Daniel6646 <76101943+Daniel6646@users.noreply.github.com>
2022-05-16 23:32:15 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,5 @@
+insert into alien(aid,aname,tech) value(101,'Daniel','Java');
+insert into alien(aid,aname,tech) value(102,'Zues','Python');
+insert into alien(aid,aname,tech) value(103,'Loki','Science');
+insert into alien(aid,aname,tech) value(104,'Aphrotide','Maths');
+insert into alien(aid,aname,tech) value(105,'Fenrir','SPRING');
